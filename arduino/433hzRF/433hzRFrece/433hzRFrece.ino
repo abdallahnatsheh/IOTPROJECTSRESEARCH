fa4422debebfc36548582003d77c1cba15860a3f
EHAuthority <29822416+abdallahnatsheh@users.noreply.github.com>
2021-05-20 20:55:51 +0300

    Add files via upload

---
+++ arduino/433hzRF/433hzRFrece/433hzRFrece.ino
@@ -0,0 +1,45 @@
+
+#include <VirtualWire.h>
+const int rx_pin = 11;
+
+void setup()
+{
+  Serial.begin(9600);  // Debugging only
+  Serial.println("initializing....");
+ 
+  vw_set_rx_pin(rx_pin);
+  vw_setup(2000);      // Bits per sec
+  vw_rx_start();       // Start the receiver PLL running
+  Serial.println("done");
+}
+
+void loop()
+{
+
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  vw_wait_rx_max(2000);
+ 
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    int i;
+
+ //   digitalWrite(LED_BUILTIN, HIGH); // Flash a light to show received good message
+    // Message with a good checksum received, dump it.
+    Serial.print("Got: ");
+
+    for (i = 0; i < buflen; i++)
+    {
+      Serial.print(buf[i]);
+      Serial.print(" ");
+    }
+    Serial.println("");
+   // digitalWrite(LED_BUILTIN, LOW);
+  }
+
+ Serial.println("Bad messages received:");// Serial.println(vw_get_rx_bad());
+ // Serial.println("Good messages received:"); //Serial.println(vw_get_rx_good());
+
+  delay(1000);
+}
